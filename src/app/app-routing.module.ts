0630c7e2fb130e00cbbe358a8a8b6c3e8cad40bf
givera <giveraango@gmail.com>
2023-12-29 18:34:52 +0100

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+
+const routes: Routes = 
+  [
+    {path : '', component : AccueilComponent},
+    {path: 'perdu',  loadChildren: () => import('./perdu/perdu.module').then(m => m.PerduModule) },
+    {path: 'trouve',  loadChildren: () => import('./trouve/trouve.module').then(m => m.TrouveModule) },
+
+  ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
